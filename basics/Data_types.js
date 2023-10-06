877d115a827d36ebd0c7ad84e18c8f372e4adaff
lovedeep <106065832+Love-deep@users.noreply.github.com>
2023-10-06 05:13:34 +0000

    data types

---
+++ basics/Data_types.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all js code as newer version so why we use strict
+
+//alert(3+3) //we are using nodejs,not in browser
+
+console.log(2+7)
+//us new line to write code fr bettr readability
+console.log("lovedeep"); // use ; or not not a issue in javascipt
+
+//Data types
+
+let name="Lovedeep" //string
+let age=20 //integer
+let isloggedIn=false //boolean
+
+//number =>2 to the power 53
+//bigint - if number is very large
+//string => " " or '' prefer doble " "
+//boolean => true/false
+//null =>  standalone value
+//indefined => when we declare variable but doesnt give value
+//Symbol => to find unique
+
+//object
+
+console.log(typeof name);
+console.log(typeof null); //output is object
+console.log(typeof undefined); //output is undefined because its a type 
