a98365f904234487b45d9906c0273458401ebb0d
lovedeep <106065832+Love-deep@users.noreply.github.com>
2023-10-06 05:47:08 +0000

    conversion of data type

---
+++ basics/conversionOperation.js
@@ -0,0 +1,52 @@
+/*let score="23ab"
+console.log(typeof(score)); //string
+console.log(typeof score);  //both method are same
+  
+
+let valueInNumber=Number(score)  
+console.log(typeof valueInNumber)  //number
+//to check which number comes
+console.log(valueInNumber)
+
+//"33\"=> 33
+//"33abc" => NaN
+//true=1 false=0
+
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=> true; 0=> false;
+//""=>false
+//"love"=>true
+
+*/
+
+/******************* Operations ***********************/
+let value=3
+let negvalue=-value
+console.log(negvalue)
+
+//add to string
+
+let str1="Love"
+let str2=" deep"
+
+let str3=str1+str2
+console.log(str3)
+
+//understand this if string first then all 
+//are treated as string etc..
+console.log("1"+2) //12
+console.log("1"+"2")//12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2+"3") //33
+
+console.log(true) //true
+console.log(+true) //we increment but as there is no value so it gives 1
+console.log(+"")  //0
+
+let gamecounter=10
+++gamecounter             //read prefix and postfix
+console.log(gamecountercounter)
