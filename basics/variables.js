a91ad5992c6d6c252c1c836815545c432f804c60
lovedeep <106065832+Love-deep@users.noreply.github.com>
2023-10-06 04:55:49 +0000

    variables declaration

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountid=123456
+let accountemail="lovedeep@gmail.com"
+var accountpass="1235214"
+accountCity="Hoahiarpur"
+let accountstate;
+console.log(accountid)
+console.table([accountid,accountemail,accountpass,accountCity,accountstate])
+
+/*
+const is used for contant storing purpose
+var and let is used for variables but var is not
+much use now because of issue in block scope and functional scope
+Prefer let
+we can declare variable without defining var and let as in accountCity 
+above but its bad practice
+if we dont declare value then it show undefined as in account state
+
+*/
