8f2968fa4b30f327fec0d3ae9afc7646c58cd57e
lovedeep <106065832+Love-deep@users.noreply.github.com>
2023-10-07 11:01:17 +0000

    dates and time

---
+++ basics/dates_and_time.js
@@ -0,0 +1,14 @@
+//Dates
+ 
+//let mydate=new Date()
+//console.log(mydate)
+//console.log(mydate.toString())
+//console.log(mydate.toDateString())
+//console.log(mydate.toLocaleDateString())
+//console.log(typeof mydate)
+//
+//let myCreatedDate=new Date(2023,6,23)
+//console.log(myCreatedDate.toDateString()) //months are started from 0 0-jan and so on...
+
+let newdate=new Date()
+console.log(newdate.getDay()+1)
