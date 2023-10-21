2b2abf99a73758d36f5671250d002db5b28fbb29
lovedeep <106065832+Love-deep@users.noreply.github.com>
2023-10-21 06:26:33 +0000

    changes

--- basics_01/dates_and_time.js
+++ basics_01/dates_and_time.js
@@ -1,30 +1,29 @@
 //Dates:- date is object in javascipt
  
-//let mydate=new Date()
-//console.log(mydate)
-//console.log(mydate.toString())
-//console.log(mydate.toDateString())
-//console.log(mydate.toISOString())
-//console.log(mydate.toLocaleString())
-//console.log(mydate.toUTCString())
-//console.log(mydate.toTimeString())
-//console.log(mydate.toLocaleDateString())
-//console.log(typeof mydate)
-//
-//let myCreatedDate=new Date(2023,6,23)
-//
-//console.log(myCreatedDate.toDateString()) //months are started from 0 0-jan and so on...
-//
-//let newdate=new Date()
-//console.log(newdate.getDay()+1)
-//
-//let createdate=new Date(2023,0,23,5,9)
-//console.log(createdate.toLocaleString()) 
-//
-//
-//let date=new Date("2023-01-13") // here 00 month is not valid dqte so start with 01
-//console.log(date.toLocaleString()) 
+let mydate=new Date()
+console.log(mydate)
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toISOString())
+console.log(mydate.toLocaleString())
+console.log(mydate.toUTCString())
+console.log(mydate.toTimeString())
+console.log(mydate.toLocaleDateString())
+console.log(typeof mydate)
 
+let myCreatedDate=new Date(2023,6,23)
+
+console.log(myCreatedDate.toDateString()) //months are started from 0 0-jan and so on...
+
+let newdate=new Date()
+console.log(newdate.getDay()+1)
+
+let createdate=new Date(2023,0,23,5,9)
+console.log(createdate.toLocaleString()) 
+
+
+let date=new Date("2023-01-13") // here 00 month is not valid dqte so start with 01
+console.log(date.toLocaleString())
 let date001=new Date("01-13-2023") // above and this have same output
 console.log(date001.toLocaleString()) 
 
