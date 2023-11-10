212b330df678992df3f9873458cf58749394cf16
lovedeep <106065832+Love-deep@users.noreply.github.com>
2023-11-10 12:20:31 +0000

    forof loop

---
+++ iteration_05/three.js
@@ -0,0 +1,34 @@
+//for of
+
+['','',''] //array ke andr string
+[{},{}]   //array ke andr objects
+
+const arr=[1,2,3,4,5]    //num is iterator 
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings='hello world'
+for (const iterator of greetings) {
+    //console.log(`each char is ${iterator}`);
+}
+
+
+//MAPS
+
+const map= new Map
+map.set("In","India")
+map.set("PK","Pakistan")
+map.set("NP","Nepal")
+map.set("CH","Chandiagrh")
+//console.log(map);
+
+for (const key of map) {
+    console.log(key); //print in array 
+    
+}
+for (const [key,value] of map) {
+    console.log(key,':-',value); //print in array 
+    
+}
+
